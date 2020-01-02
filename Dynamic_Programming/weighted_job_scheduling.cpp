79a839fbad7f0e681c304cc86c94523611ad6a6d
Neil Pradhan <34929271+neilpradhan@users.noreply.github.com>
2020-01-02 21:57:30 +0100

    Update weighted_job_scheduling.cpp

--- Dynamic_Programming/weighted_job_scheduling.cpp
+++ Dynamic_Programming/weighted_job_scheduling.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
-
+// O(N^2)
 class job{
     public:
     int start;
@@ -24,6 +24,11 @@ bool compare(job j1, job j2){
     return j1.finish<j2.finish;
 }
 
+int binary_search(job j,int index){
+    
+}
+
+
 
 int main()
 {
@@ -43,33 +48,54 @@ int main()
     //     cout<<"\n";
     // }
     
-    int dp[n];
+    int* dp = new int[n];
+    dp[0] = j[0].profit;
     
-    for(int i =0;i<n;i++){
-        int c = 0;
+    // for(int i = 1;i<n;i++){
+    //     // dp[i] = j[i].profit;
+    //     dp[i] = 0;
+    // }
+    
+    for(int i =1;i<n;i++){
+        int including = j[i].profit;
+        int index_of_latest_conflict = -1;
         for (int k=i-1;k>=0;k--){
             
             if (j[k].finish<=j[i].start){
                 // jobs available opportunities
-                c = max(j[k].profit,c);
+                // non overlapping
+                // we find latest non conflicting
+                index_of_latest_conflict  = k;
+                break;     
+            }
+
+        }
+            if(index_of_latest_conflict!=-1){
+            including+= dp[index_of_latest_conflict];
                 
             }
-        }
-        dp[i] = j[i].profit + c ;
+
+        dp[i] = max(including,dp[i-1]);
+        
     }
     
+    
+    int ans = dp[n-1];
+    delete [] dp; 
     // for(int i = 0;i<n;i++){
     //     cout<<dp[i]<<" ";
     // }
-    int maximum =0;
-    for(int i = 0;i<n;i++){
-        if (dp[i]>=maximum){
-            maximum = dp[i];
-        }
-    }
-    cout<<maximum;
+    // int maximum =0;
+    // for(int i = 0;i<n;i++){
+    //     if (dp[i]>=maximum){
+    //         maximum = dp[i];
+    //     }
+    // }
+    // cout<<maximum;
     
     
-
+    cout<<ans;
+    
+    //Write your code here
     return 0;
 }
