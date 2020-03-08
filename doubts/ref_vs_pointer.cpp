06501cefd35c6be7f44dd0ad87459cd483a8aff8
Neil Pradhan <34929271+neilpradhan@users.noreply.github.com>
2020-03-08 16:25:19 +0100

    Create ref_vs_pointer.cpp

---
+++ doubts/ref_vs_pointer.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void func(vector<int>&p){
+    cout<<p[0]<<endl;
+    
+}
+
+int main()
+{
+//   cout << "Hello World" << endl; 
+   // list of connections
+   
+   
+ vector<int>*pointer_to_vect = new vector<int>;
+ 
+ pointer_to_vect->push_back(24);
+ 
+ vector<int> v = {1,2};
+ 
+ 
+ 
+//  func(v);
+ 
+ func(pointer_to_vect);
+ 
+ 
+   
+   
+   
+   
+   
+   
+   
+   return 0;
+}
